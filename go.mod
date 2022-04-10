edf6ef308b0850c25a1ce468ec354d8ce0f50b68
wsjcko <wsjcko@163.com>
2022-04-10 17:26:19 +0800

    param -ip

--- go.mod
+++ go.mod
@@ -5,16 +5,23 @@ go 1.17
 require (
 	github.com/asim/go-micro/plugins/config/source/consul/v4 v4.0.0-20220404185419-6dedee5d8c2c
 	github.com/asim/go-micro/plugins/registry/consul/v4 v4.0.0-20220404185419-6dedee5d8c2c
+	github.com/asim/go-micro/plugins/wrapper/ratelimiter/uber/v4 v4.0.0-20220407104607-62c2981baf63
+	github.com/asim/go-micro/plugins/wrapper/trace/opentracing/v4 v4.0.0-20220407104607-62c2981baf63
 	github.com/golang/protobuf v1.5.2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/uber/jaeger-client-go v2.30.0+incompatible
+	github.com/urfave/cli/v2 v2.3.0
 	go-micro.dev/v4 v4.2.1
 	google.golang.org/protobuf v1.26.0
 )
 
 require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/Microsoft/go-winio v0.5.0 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
 	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
 	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
@@ -50,8 +57,10 @@ require (
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/ratelimit v0.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/asim/go-micro/plugins/registry/consul/v4"
+	ratelimit4 "github.com/asim/go-micro/plugins/wrapper/ratelimiter/uber/v4"
+	opentracing4 "github.com/asim/go-micro/plugins/wrapper/trace/opentracing/v4"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/opentracing/opentracing-go"
+	cli2 "github.com/urfave/cli/v2"
 	"github.com/wsjcko/shopcategory/common"
 	"github.com/wsjcko/shopcategory/domain/repository"
 	"github.com/wsjcko/shopcategory/domain/service"
@@ -15,23 +19,63 @@ import (
 )
 
 var (
-	MICRO_SERVICE_NAME = "go.micro.service.shop.category"
-	MICRO_VERSION      = "latest"
-	MICRO_ADDRESS      = "127.0.0.1:8086"
+	MICRO_SERVICE_NAME   = "go.micro.service.shop.category"
+	MICRO_VERSION        = "latest"
+	MICRO_ADDRESS        = "0.0.0.0:8086"
+	MICRO_QPS            = 100
+	DOCKER_HOST          = "127.0.0.1"
+	MICRO_CONSUL_ADDRESS = "127.0.0.1:8500"
+	MICRO_JAEGER_ADDRESS = "127.0.0.1:6831"
 )
 
+func SetDockerHost(host string) {
+	DOCKER_HOST = host
+	MICRO_CONSUL_ADDRESS = host + ":8500"
+	MICRO_JAEGER_ADDRESS = host + ":6831"
+}
+
 func main() {
+	function := micro.NewFunction(
+		micro.Flags(
+			&cli2.StringFlag{ //micro 多个选项 --ip
+				Name:  "ip",
+				Usage: "docker Host IP(ubuntu)",
+				Value: "0.0.0.0",
+			},
+		),
+	)
+
+	function.Init(
+		micro.Action(func(c *cli2.Context) error {
+			ipstr := c.Value("ip").(string)
+			if len(ipstr) > 0 { //后续校验IP
+				log.Info("docker Host IP(ubuntu)1111", ipstr)
+			}
+			SetDockerHost(ipstr)
+			return nil
+		}),
+	)
+
+	log.Info("DOCKER_HOST ", DOCKER_HOST)
+
 	//配置中心
-	consulConfig, err := common.GetConsulConfig("127.0.0.1", 8500, "/micro/config")
+	consulConfig, err := common.GetConsulConfig(MICRO_CONSUL_ADDRESS, "/micro/config")
 	if err != nil {
 		log.Error(err)
 	}
 	//注册中心
 	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
 		options.Addrs = []string{
-			"127.0.0.1:8500",
+			MICRO_CONSUL_ADDRESS,
 		}
 	})
+	//链路追踪
+	t, io, err := common.NewTracer(MICRO_SERVICE_NAME, MICRO_JAEGER_ADDRESS)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
 
 	// Create service
 	srv := micro.NewService(
@@ -41,13 +85,17 @@ func main() {
 		micro.Address(MICRO_ADDRESS),
 		//添加consul 作为注册中心
 		micro.Registry(consulRegistry),
+		//绑定链路追踪 服务端绑定handle 客户端绑定client
+		micro.WrapHandler(opentracing4.NewHandlerWrapper(opentracing.GlobalTracer())),
+		//添加限流
+		micro.WrapHandler(ratelimit4.NewHandlerWrapper(MICRO_QPS)),
 	)
 
 	//获取mysql配置,路径中不带前缀
 	mysqlInfo := common.GetMysqlFromConsul(consulConfig, "mysql")
 
 	//连接数据库
-	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+"@/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+"@tcp("+mysqlInfo.Host+":"+mysqlInfo.Port+")/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		log.Error(err)
 	}
